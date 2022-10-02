a45f7fbe94f9d8d55ed52efad5131a8ae1584ee1
Andrej Schwanke <andrejschwanke19@gmail.com>
2022-10-02 14:25:04 +0200

    added a new front page

--- js/typewriter.js
+++ js/typewriter.js
@@ -2,15 +2,35 @@
 Based on: https://www.w3schools.com/howto/howto_js_typewriter.asp
 */
 var i = 0;
-var txt = 'Programming - Artificial intelligence - Bioinformatics'; /* The text */
+var subheading = 'Programming - Artificial intelligence - Data Science'; /* The text */
 var speed = 70; /* The speed/duration of the effect in milliseconds */
+var finisched = false
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+  if (i < subheading.length) {
+    document.getElementById("typewriter").innerHTML += subheading.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+  } else {
+    finisched = true
   }
-}
-window.addEventListener('load', typeWriter)
 
+}
+
+
+var j = 0;
+var c1 = 'Hey! I am a computer science student from Freiburg!'
+
+function aboutmecontent(){
+    if(!finisched) {return}
+    if(j < c1.length){
+      document.getElementById("aboutmecontent").innerHTML += c1.charAt(j);
+      j++
+    } else {
+      const hiddenElements = document.querySelectorAll('.hidden-img');
+      hiddenElements.forEach((el) => el.classList.add('show-img'));
+    }
+}
+
+window.addEventListener('load', typeWriter)
+setInterval(aboutmecontent,speed)
